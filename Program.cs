1b78944d222f7ea3abe4a17878ee2e2d2c27fa65
Vitor Guedes <vitorgui01@gmail.com>
2025-08-24 23:03:49 -0300

    estacionamento DIO

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿﻿using System;
+using DesafioFundamentos.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Seja bem-vindo ao sistema de estacionamento!\n");
+
+        Console.WriteLine("Digite o preço inicial:");
+        decimal precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Digite o preço por hora:");
+        decimal precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+        Estacionamento estacionamento = new Estacionamento(precoInicial, precoPorHora);
+
+        string opcao = string.Empty;
+        bool exibirMenu = true;
+
+        while (exibirMenu)
+        {
+            Console.WriteLine("Digite a sua opção:");
+            Console.WriteLine("1 - Cadastrar veículo");
+            Console.WriteLine("2 - Remover veículo");
+            Console.WriteLine("3 - Listar veículos");
+            Console.WriteLine("4 - Encerrar");
+
+            opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    estacionamento.AdicionarVeiculo();
+                    break;
+
+                case "2":
+                    estacionamento.RemoverVeiculo();
+                    break;
+
+                case "3":
+                    estacionamento.ListarVeiculos();
+                    break;
+
+                case "4":
+                    exibirMenu = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("O programa se encerrou.");
+    }
+}
